ccb94e2895347be98e1188d06a87a232172040ed
sinan muhammed <sinanmuhammed0260@gmail.com>
2026-07-15 12:40:44 +0530

    javascipt basics

---
+++ basics.js
@@ -0,0 +1,12 @@
+console.log("hello")
+// let name="sinan"
+// let name=prompt("enter your  name")
+// console.log(name)
+ let a=parseInt(prompt("enter the value of a"))
+ let b=parseInt(prompt("enter the value of b"))
+let result=parseInt(a) + parseInt(b)
+console.log(result) 
+result=parseInt(a)-parseInt(b)
+console.log(result)
+
+
