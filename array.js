1a679b4eeb79c929361fe1d8c0d916f49e1cdae2
sinan muhammed <sinanmuhammed0260@gmail.com>
2026-07-17 12:32:23 +0530

    javascript splice statement

---
+++ array.js
@@ -0,0 +1,8 @@
+let names=["sinan","sidan","shemil"]
+
+let n=prompt("enter the position to add")
+let d=prompt("enter the element to delete")
+let t=prompt("enter the text")
+names.splice(n,d,t)
+console.log(names)
+
---
+++ swapp3.js
@@ -0,0 +1,11 @@
+let a=prompt("enter value A")
+let b=prompt("enter value B")
+let c=prompt("enter value C")
+console.log(a,b,c)
+let temp=a
+a=c
+c=b
+b=temp
+
+
+alert("the value of A is"+a +"\n the value of B is"+b + "\n the value of c"+c)
