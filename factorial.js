67b1432e37a30f35e01fb389d6bae69c94b908bb
sinan muhammed <sinanmuhammed0260@gmail.com>
2026-07-17 15:30:00 +0530

    even or odd problem

---
+++ factorial.js
@@ -0,0 +1,5 @@
+let num=parseInt(prompt("enter the number"))
+let num2
+for()
+
+
---
+++ if_else.js
@@ -0,0 +1,8 @@
+let number=parseInt(prompt(" enter the number"))
+if(number%2==0){
+console.log("even")
+}
+else{
+    console.log("odd")
+    
+}
