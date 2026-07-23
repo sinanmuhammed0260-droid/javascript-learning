6c498a128d81cce5f090304ee0d7418298f0e6dc
sinan muhammed <sinanmuhammed0260@gmail.com>
2026-07-23 12:57:31 +0530

    array search and position problem

--- array2.js
+++ array2.js
@@ -1,12 +1,22 @@
+
 let num=[10,20,30,40]
 alert(num)
-let srch=parseInt(prompt("enter the number to be searched"))
+let num2=parseInt(prompt('enter the number to be search'))
+let n=false
+
 for(i=0;i<num.length;i++){
-    console.log(num[i]);
     
-}
-
-
+    if(num2==num[i]) {
+        n=true
+        alert("number is found"+"\n position is" +i)
+        
+        }  
+        
+    
+    }
+      if(n==false){
+        alert("no not found")
+      }
 
 
 
