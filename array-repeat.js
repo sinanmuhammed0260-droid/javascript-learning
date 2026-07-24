de2d24cb92ce4e7d2148d36ba4d15c0c1ae7db24
sinan muhammed <sinanmuhammed0260@gmail.com>
2026-07-24 16:41:35 +0530

    repeated number count in an array

---
+++ array-repeat.js
@@ -0,0 +1,11 @@
+let a=[10,20,10]
+let num=parseInt(prompt("enter a number"))
+let count=0
+for(i=0;i<a.length;i++){
+    if(num==a[i]){
+        count=count+1
+    }
+    
+}
+console.log(count);
+
